449698121bf0af7051e73e0e73e0d8c3f0eaaafc
juan.damato <juan.damato@epicio.com>
2025-09-30 18:38:09 -0300

    update

--- main_parallel_capture.py
+++ main_parallel_capture.py
@@ -1,6 +1,7 @@
 import cv2
 import threading
 import numpy as np
+import os
 from ultralytics import YOLO
 import ollama
 import base64
@@ -11,7 +12,6 @@ import time
 import argparse
 import os
 import sys
-
 from flask import Flask, Response, request, jsonify
 from flask_cors import CORS
 
@@ -21,25 +21,31 @@ from flask_cors import CORS
 ########################################################
 app = Flask(__name__)
 CORS(app)  # allow all origins
-
 # Number of cameras
-NUM_CAMERAS = 4
+NUM_CAMERAS = 8
 
 # Camera indexes (adjust according to your system)
-CAMERA_IDS = [0, 1, 2, 3]
+CAMERA_IDS = [0, 1, 2, 3, 4, 5, 6, 7]
+ENABLED_CAMERAS = [False,False,False,False,False,False,False,False]
 
 # Dictionary to store the frames
 frames = {i: None for i in CAMERA_IDS}
+output_frames = [None, None]
 
 # Lock for thread safety
 lock = threading.Lock()
+
+RESIZE_CAM_WIDTH = 640
+RESIZE_CAM_HEIGHT = 480
 
 
 # Queue for frames to be saved
 frame_queue = queue.Queue()
 stop_saver = False
-
-
+threads = []
+last_response = ""
+chunks = []
+best_frame = 0
 ### Define COCO keypoint pairs for skeleton connections
 SKELETON_EDGES = [
     (5, 7), (7, 9),      # Left arm: shoulder → elbow → wrist
@@ -60,23 +66,13 @@ SKELETON_COLOR_EDGES = [
 ]
 CONF_THRESHOLD = 0.5  # confidence threshold (0-1)
 
-actions_list =[ "0.Preparing room",    
- "1. Cleanning with duster",
- "2. Cleaning surfaces with cloth",
- "3. Cleaning Bathroom (out of scene)",
- "4. Replace trash bags",
- "5. Replace bed sheets",
- "6. Mopping" ]
-
-output_frames = [None, None]
-
-RESIZE_CAM_WIDTH = 640
-RESIZE_CAM_HEIGHT = 480
+os.makedirs("./temp/", exist_ok=True)
 
 # Open a log file once for the whole program
 log_file = open("./temp/frames_log.txt", "w")
 log_lock = threading.Lock()
 
+###################################################################3
 def log_message(message):
     """
     Logs a message to console and to a text file (thread-safe).
@@ -87,7 +83,7 @@ def log_message(message):
         log_file.flush()
 
 
-
+##############################################################################
 class TrackedPerson:
     def __init__(self, keypoints, confidences, track_id):
         self.keypoints = keypoints
@@ -221,7 +217,9 @@ def send_image_to_VLLM( image , index):
         response = ollama.chat(model='gemma3:4b', messages=messages)
 
         # Print the model's response
-        log_message("-------"+str(index)+"---------------"+response['message']['content'])
+        log_message("-------" + str(index) + "---------------" + response['message']['content'])
+        
+        last_response = response['message']['content']
         print(f"Ollama processing time: {time.time() - now} ")
        # cv2.imshow("ollama", image)
     except Exception as e:
@@ -301,30 +299,43 @@ def compute_best_crop(persons,frames,img_small, frame_index):
       #  cv2.imshow("best_Frame", crop)
         crop_img= frames[best_frame][max(0,y-window):y+h+window, max(0,x-window):x+w+window]
     
-    return crop, best_frame, crop_img
+    return crop, best_frame, crop_img, best_conf
 
 
 #####################################
 ## capture
 def capture_camera(src, camera_id , findex):
 
+    blank = False
     cap = cv2.VideoCapture(src)
     if not cap.isOpened():
-        print(f"Camera {camera_id} not available")
-        return
-    print(f"Camera {camera_id}  available")
+        print(f"Camera {camera_id} not available. Using blank camera")
+        ENABLED_CAMERAS[camera_id] = False
+        blank = True
+        cap = None
+        
+    else:
+        print(f"Camera {camera_id}  available")
    # cap.set(cv2.CAP_PROP_POS_FRAMES, findex)
+        ENABLED_CAMERAS[camera_id] = True
     ###      
     while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
+
+        if blank:
+            frame = np.zeros((RESIZE_CAM_HEIGHT, RESIZE_CAM_WIDTH, 3), np.uint8)
+            milliseconds_to_sleep = 25
+            time.sleep(milliseconds_to_sleep / 1000)
+        else:
+            ret, frame = cap.read()
+            if not ret:
+                break
         frames[camera_id] = frame
         milliseconds_to_sleep = 10
         time.sleep(milliseconds_to_sleep / 1000)
-       # cv2.waitKey(1)
 
-    cap.release()
+       # cv2.waitKey(1)
+    if cap is not None:
+        cap.release()
 
 #####################################################33
 # Start a thread for each camera
@@ -366,24 +377,32 @@ def detect_persons(model, tracker, img_small, findex):
     crop = None
     
 
-    crop, best_frame, crop_img = compute_best_crop(persons, frames, img_small, findex)
+    crop, best_frame, crop_img, best_conf = compute_best_crop(persons, frames, img_small, findex)
 
-    return persons, crop_img , best_frame
+    return persons, crop_img , best_frame, best_conf
         
 ####################################################333
 def parallel_combine_videos(video_files,freq_ollama):
-    global stop_saver
+    global stop_saver, threads
+    
+    if not os.path.exists("yolo11m-pose.pt"):
+        model = YOLO("yolo11m-pose.pt")
+        model.export(format="onnx")
+
      # Load YOLO pose model (use 'yolov8s-pose.pt' for better accuracy)
-    model = YOLO("yolov8l-pose.pt")
+    model = YOLO("yolo11m-pose.pt")
     tracker = "botsort.yaml"
 
-    threads = []
+   
     findex = 1000
 
      # Start background thread
     saver_thread = threading.Thread(target=frame_saver_worker, daemon=True)
     saver_thread.start()
-
+   ##############################################################
+    
+    chunks =[ [0, 1, 2, 3] ,  [4, 5, 6, 7] ]
+    
     for cam_id,cam_source in enumerate(video_files):
         t = threading.Thread(target=capture_camera, args=(cam_source, cam_id,findex))
         t.daemon = True
@@ -394,39 +413,49 @@ def parallel_combine_videos(video_files,freq_ollama):
 
     accumulated_detections = 0
     while True:
-        # Make sure we have all frames
-        if all(frames[cam_id] is not None for cam_id in CAMERA_IDS):
-            # Resize frames for display (e.g., 320x240 each)
-            resized = [cv2.resize(frames[cam_id], (640, 480)) for cam_id in CAMERA_IDS]
 
-            # Combine into 2x2 grid
-            top = cv2.hconcat([resized[0], resized[1]])
-            bottom = cv2.hconcat([resized[2], resized[3]])
-            combined = cv2.vconcat([top, bottom])
+        for idCh, chunk in enumerate(chunks):
 
-            persons, crop, best_frame = detect_persons(model, tracker, combined, findex)
-                                
+            best_conf = 0
+            best_crop = None
+            best_frame = None
 
-            for pk in persons:
-                pk.draw(combined)
+            # Make sure we have all frames of this chunk
+            if all(frames[cam_id] is not None for cam_id in chunk):
+                # Resize frames for display (e.g., 320x240 each)
+                resized = [cv2.resize(frames[cam_id], (RESIZE_CAM_WIDTH, RESIZE_CAM_HEIGHT)) for cam_id in chunk]
 
-            cv2.putText(combined, f"id:{findex}", (int(20),int(20) ), 1, 0.75, (255, 255, 255))
-    
-            cv2.imshow("YOLO + SORT Tracking", combined)
-           # print (f"update {findex}")
+                # Combine into 2x2 grid
+                top = cv2.hconcat([resized[0], resized[1]])
+                bottom = cv2.hconcat([resized[2], resized[3]])
+                combined = cv2.vconcat([top, bottom])
 
-            if accumulated_detections > freq_ollama and crop is not None:
-                frame_queue.put((findex, frames[best_frame].copy()))
-                accumulated_detections = 0
+                persons, crop, sel_frame, conf = detect_persons(model, tracker, combined, findex)
+                                    
+                if conf > best_conf:
+                    best_conf = conf
+                    best_crop = crop
+                    best_frame =resized[ sel_frame]
+
+                for pk in persons:
+                    pk.draw(combined)
+
+                cv2.putText(combined, f"id:{findex}", (int(20),int(20) ), 1, 0.75, (255, 255, 255))
         
-            # Draw keypoints and skeleton
-            #annotated_frame = results[0].plot()
-            if crop is not None and (crop.shape[0] * crop.shape[1]) > 0:
-                cv2.imshow("crop", crop)
+                cv2.imshow(f"YOLO + SORT Tracking {idCh}", combined)
+            # print (f"update {findex}")
+
+                if accumulated_detections > freq_ollama and crop is not None:
+                    frame_queue.put((findex, frames[best_frame].copy()))
+                    accumulated_detections = 0
+            
+            
+                # Draw keypoints and skeleton
+                #annotated_frame = results[0].plot()
+            if best_crop is not None and (best_crop.shape[0] * best_crop.shape[1]) > 0:
+                cv2.imshow(f"crop ", best_crop)
                 accumulated_detections += 1
-            
-            output_frames[0] = frames[best_frame].copy()
-            
+            output_frames[0] = best_frame
             k = cv2.waitKey(20) 
             if k & 0xFF == ord("q"):
                 break
@@ -445,6 +474,12 @@ def parallel_combine_videos(video_files,freq_ollama):
         cap.release()
     cv2.destroyAllWindows()
 
+@app.route("/vllm")
+def get_vllm_response():
+  
+  
+    return jsonify({ "vllm": last_response})
+    
 
 def generate_frames(idSource=1):
    
@@ -464,7 +499,6 @@ def generate_frames(idSource=1):
         frame_bytes = buffer.tobytes()
         milliseconds_to_sleep = 10
         time.sleep(milliseconds_to_sleep / 1000)
-            
         yield (b'--frame\r\n'
             b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
 
@@ -493,49 +527,46 @@ def video_feed(idx):
                         mimetype="multipart/x-mixed-replace; boundary=frame")
    
 
+
 ######################################################################
 def parse_args():
     # Create ArgumentParser object
     parser = argparse.ArgumentParser(description="Parse command-line arguments for video processing.")
    # Define command-line arguments with optional flags
     parser.add_argument("--input_videos", default = "E:/Resources/Novathena/multicam_2/", help="Path to the input file")
-    parser.add_argument("--freq_ollama", default=100, help="Freq to call Ollama")
+    parser.add_argument("--freq_ollama", default =100, help="Freq to call Ollama")
     parser.add_argument("--port", default = "5101", help="Port for exposing")
- 
     # Parse the arguments
     args = parser.parse_args()
 
     # Return the parsed arguments
     return args
 
-def run_app(port):
-        ### start parallel processors
-    #for cam_id,cam_source in enumerate(sources):
-    app.run(host="0.0.0.0", port=port, debug=False)
-
-
 if __name__ == "__main__":
     args = parse_args()
 
-    print ("-------------------------------------------------------")
-    print("Version of parallel combined Cameras . 30set2025 vA")
-    print ("-------------------------------------------------------")
     if sys.platform == 'win32':
         print("Running on Windows")
         path = "E:/Resources/Novathena/multicam_2/"
-        video_files = [path +"cam1_trimmed.mp4", path +"cam2_trimmed.mp4", path +"cam3_trimmed.mp4", path +"cam4_trimmed.mp4"]
+        video_files = [path + "cam1_trimmed.mp4", path + "cam2_trimmed.mp4",
+                        path + "cam3_trimmed.mp4", path + "cam4_trimmed.mp4", path + "cam6.mp4", path + "cam7.mp4",
+                        ## fill with empty
+                        "blank", "blank"]
     else:
         video_files =  [ "rtsp://root:Nova2022**@10.10.1.107:554/axis-media/media.amp?streamprofile=stream1",
                         "rtsp://root:Nova2022**@10.10.1.103:554/axis-media/media.amp?streamprofile=stream1",
                     "rtsp://root:Nova2022**@10.10.1.106:554/axis-media/media.amp?streamprofile=stream1",
                     "rtsp://root:Nova2022**@10.10.1.105:554/axis-media/media.amp?streamprofile=stream1"]
   
-    t = threading.Thread(target=run_app, args=(int(args.port),  ))
+    print ("-------------------------------------------------------")
+    print("Version of parallel combined Cameras . 30set2025 version chunks")
+    print("-------------------------------------------------------")
+    
+    ### start parallel processors
+    #for cam_id,cam_source in enumerate(sources):
+    t = threading.Thread(target=parallel_combine_videos, args=(video_files, int(args.freq_ollama), ))
     t.daemon = True
     t.start()
-    
+    threads.append(t)
 
-
-    parallel_combine_videos(video_files, int(args.freq_ollama))
-    
-    
+    app.run(host="0.0.0.0", port=args.port, debug=False)
